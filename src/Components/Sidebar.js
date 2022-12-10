e0a03e63f2c33acbc74a6f251ae3b1dbb7064334
Arunima Prajapati <110831349+arunima1200@users.noreply.github.com>
2022-12-10 08:39:56 +0530

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,21 +1,21 @@
 import React from 'react'
-// import Header from './Header';
 import './Sidebar.css';
 import Cities from './Cities';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, NavLink, Routes} from 'react-router-dom';
+import Header from './Header';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Router>
-        <ul className='side-list'>
-            <li><Link to="" className='link'>Home</Link></li>
-            <li><Link to="/cities" className='link'>Cities</Link></li>
-            <Routes>
-              <Route path="/" element={<div></div>}/> 
-              <Route path="/cities" element={<Cities/>}/>
-            </Routes>
-        </ul>
+      <ul className='side-list'>
+        <li><NavLink to="/" className={(navData) => (navData.isActive ? 'active-home active' : 'link')} >Home</NavLink></li>
+        <li><NavLink to="/cities" className={(navData) => (navData.isActive ? 'active-city active' : 'link')} >Cities</NavLink></li>
+        <Routes>
+          <Route path="/" element={<Header/>} />
+          <Route path="/cities" element={<Cities />} />
+        </Routes>
+      </ul>
       </Router>
     </div>
   )
