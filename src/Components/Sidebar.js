6fed0be7e0a5a43f1a06682712f983cb6e969f24
Arunima Prajapati <110831349+arunima1200@users.noreply.github.com>
2023-01-05 19:26:35 +0530

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import './Sidebar.css';
-import Cities from './Cities';
-import {BrowserRouter as Router, Route, NavLink, Routes} from 'react-router-dom';
-import Header from './Header';
+import React, { useEffect, useState } from "react";
+import './Sidebar.css'
+import { Link, useLocation, useParams } from "react-router-dom";
+
+const initialBackgroundColor = {
+    home: "#d1b726",
+    cities: "#472978"
+};
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-      <Router>
-      <ul className='side-list'>
-        <li><NavLink to="/" className={(navData) => (navData.isActive ? 'active-home active' : 'link')} >Home</NavLink></li>
-        <li><NavLink to="/cities" className={(navData) => (navData.isActive ? 'active-city active' : 'link')} >Cities</NavLink></li>
-        <Routes>
-          <Route path="/" element={<Header/>} />
-          <Route path="/cities" element={<Cities />} />
-        </Routes>
-      </ul>
-      </Router>
-    </div>
-  )
-}
+    const [backgroundColor, setBackgroundColor] = useState(initialBackgroundColor);
+    const location = useLocation();
 
+    useEffect(() => {
+        if(location.pathname === "/") {
+            return setBackgroundColor(initialBackgroundColor);
+        }
+        if(location.pathname === "/cities") {
+            return setBackgroundColor({
+                home: "#472978",
+                cities: "#d1b726"
+            })
+        }
+    }, [location])
+    return (
+        <div className="sidebar">
+                <Link to="/" className="link" style={{backgroundColor: backgroundColor.home}}>Home</Link> 
+                <Link to="/cities" className="link" style={{backgroundColor: backgroundColor.cities}}>Cities</Link>
+        </div>
+    )
+
+}
 export default Sidebar
