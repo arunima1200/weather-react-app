78565461e8e57016605c53fcdd0f923e4e01d70b
Arunima Prajapati <110831349+arunima1200@users.noreply.github.com>
2023-01-05 19:23:29 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
-import Sidebar from './Components/Sidebar';
-
+import Home from './Components/Home';
+import Cities from './Components/Cities';
+import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 function App() {
+  const [showModal , setShowModal] =useState(false)
+  const [ cities, setCities ] = useState({
+    selected: [],
+    favourites: []
+  });
+
   return (
     <div className="App">
-      <Sidebar/>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+           <Home 
+             showModal={showModal} 
+             setShowModal={setShowModal}
+             cities={cities}
+             setCities={setCities}
+           />
+            }
+         />
+          <Route path="/cities" element={
+            <Cities
+              showModal={showModal} 
+              setShowModal={setShowModal}
+              cities={cities}
+              setCities={setCities}
+            />
+          } 
+          />
+        </Routes>
+      </BrowserRouter>  
+    </div> 
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,184 @@
+export const Cities = [
+
+    {
+
+        id: "1",
+
+        name: 'Chennai',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '2',
+
+        name: 'Coimbatore',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '3',
+
+        name: 'Bangalore',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '4',
+
+        name: 'Mumbai',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '5',
+
+        name: 'Delhi',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '6',
+
+        name: 'Kolkata',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '7',
+
+        name: 'Punjab',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '8',
+
+        name: 'Rajasthan',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '9',
+
+        name: 'Surat',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '10',
+
+        name: 'Ranchi',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '11',
+
+        name: 'Pune',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+
+    {
+
+        id: '12',
+
+        name: 'Jaipur',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '20 °C',
+
+        humidity: '52.0%',
+
+    },
+    {
+
+        id: '13',
+
+        name: 'Patna',
+
+        desc: 'Clear with periodic clouds',
+
+        temp: '12 °C',
+
+        humidity: '85.0%',
+
+    },
+];
+
