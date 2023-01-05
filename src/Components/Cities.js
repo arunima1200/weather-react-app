ff10789fe3baf2637641d53330275d1759d34680
Arunima Prajapati <110831349+arunima1200@users.noreply.github.com>
2023-01-05 19:24:31 +0530

    Update Cities.js

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from './Sidebar';
 import './Cities.css';
+import CitiesModal from './CitiesModal';
+import CityWeatherDetail from './CityWeatherDetail';
+import SelectedCity from './SelectedCity';
 
-const Cities = () => {
+const  Cities = ({showModal ,setShowModal, cities, setCities}) => {
+  const [detailCity, setDetailCity] = useState({});
+  const handleClick = () =>{
+    setShowModal(true)
+  }
   return (
-    <div className='City-container'>
-            <h3 className='city-head'>Cities</h3>
-            <button className='city-add-btn'>+</button>
-            <hr />
-            <p className='city-msg'>You have not selected any city.</p>
+    <div className='city'>
+    <Sidebar/>
+    <div className='city-container'>
+        <div className='selected-city-header'>
+          <h3 className='city-head'>Cities</h3>
+          <button className='city-add-btn' onClick={handleClick}>+</button>
+        </div>
+        <hr/>
+        {
+          cities.selected.length ?
+          cities.selected.map(city =><SelectedCity city={city} setDetailCity={setDetailCity} />)
+          :
+          <p className='city-msg'>You have not selected any city.</p>
+        }
+    </div>
+    {
+      Object.keys(detailCity).length > 0 && <CityWeatherDetail detailCity={detailCity} cities={cities} setCities={setCities}/>
+    }
+    { showModal &&
+              <CitiesModal 
+                setShowModal={setShowModal}
+                cities={cities}
+                setCities={setCities}
+              />
+           }
     </div>
   )
 }
